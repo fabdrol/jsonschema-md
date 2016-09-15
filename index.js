20a94ea14f5241ffd09b452c4e8737d4db487749
Fabian Tollenaar <fabian@decipher.industries>
2016-09-15 21:50:56 +0200

    fixed a typo

--- index.js
+++ index.js
@@ -11,9 +11,17 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARrRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
+ *
+ * USAGE:
+ *  new Parser({
+ *    definitions: './schema/definitions.json',
+ *    entry: './schema/signalk.json',
+ *    output: './build',
+ *    debug: false
+ *  })
+ */
 
-require('./lib')
+module.exports = require('./lib/Parser')
